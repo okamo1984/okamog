2a6074970be666cc0b33999b54e76cdeea9b162a
okamo <100861134+okamo1984@users.noreply.github.com>
2022-07-18 14:05:33 +0900

    Move favicon to top directory

--- main.ts
+++ main.ts
@@ -3,9 +3,9 @@ import blog from "https://deno.land/x/blog@0.4.1/blog.tsx";
 blog({
   title: "okamog",
   author: "okamo",
-  avatar: "./okamo.jpg",
+  avatar: "okamo.jpg",
   avatarClass: "rounded-full",
-  ogImage: "./okamo.jpg",
+  ogImage: "okamo.jpg",
   links: [
     { title: "Email", url: "mailto:okamo1984@gmail.com" },
     { title: "GitHub", url: "https://github.com/okamo1984" },
