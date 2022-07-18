a14340a01115a9fcb47e4b49a9a21c59ddf5210f
okamo <100861134+okamo1984@users.noreply.github.com>
2022-07-18 12:33:45 +0900

    Update deno_blog and add favicon

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import blog from "https://deno.land/x/blog@0.3.3/blog.tsx";
+import blog from "https://deno.land/x/blog@0.4.1/blog.tsx";
 
 blog({
   title: "okamog",
@@ -12,14 +12,7 @@ blog({
     { title: "GitHub", url: "https://github.com/okamo1984" },
     { title: "Twitter", url: "https://twitter.com/okamo1984" },
   ],
-  // middlewares: [
-  //   // If you want to set up Google Analytics, paste your GA key here.
-  //   ga("UA-XXXXXXXX-X"),
-
-  //   // If you want to provide some redirections, you can specify them here,
-  //   // pathname specified in a key will redirect to pathname in the value.
-  //   redirects({
-  //     "/hello_world.html": "/hello_world",
-  //   }),
-  // ],
+  lang: "ja",
+  dateStyle: "long",
+  favicon: "favicon.ico"
 });
